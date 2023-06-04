29f5c2001a09d748ef214e545e95dfdb5ba51045
GoldenPotato <goldenpotato137@gmail.com>
2023-06-04 21:53:47 +0800

    feat: 新增单元测试项目 to：添加匹配度限制

---
+++ GalgameManager.Test/Contracts/Phrase/GalInfoPhraserTests.cs
@@ -0,0 +1,41 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using GalgameManager.Contracts.Phrase;
+
+namespace GalgameManager.Test.Contracts.Phrase;
+
+[TestFixture]
+[SuppressMessage("ReSharper", "StringLiteralTypo")]
+public class GalInfoPhraserTests
+{
+    [Test]
+    public void SimilarityTest1()
+    {
+        // Arrange
+        var s1 = "bacde";
+        var s2 = "abed";
+        // Act
+        var result = IGalInfoPhraser.Similarity(s1, s2);
+        // Console.WriteLine(result);
+        // Assert
+        if(Math.Abs(result - 0.672) > 0.001)
+            Assert.Fail();
+        else
+            Assert.Pass();
+    }
+    
+    [Test]
+    public void SimilarityTest2()
+    {
+        // Arrange
+        var s1 = "FAREMVIEL";
+        var s2 = "FARMVILLE";
+        // Act
+        var result = IGalInfoPhraser.Similarity(s1, s2);
+        //Console.WriteLine(result);
+        // Assert
+        if(Math.Abs(result - 0.88425) > 0.001)
+            Assert.Fail();
+        else
+            Assert.Pass();
+    }
+}
--- GalgameManager/Contracts/Phrase/IGalInfoPhraser.cs
+++ GalgameManager/Contracts/Phrase/IGalInfoPhraser.cs
@@ -13,4 +13,39 @@ public interface IGalInfoPhraser
     public Task<Galgame?> GetGalgameInfo(Galgame galgame);
     
     public RssType GetPhraseType();
+
+    /// <summary>
+    /// 计算两个字符串的相似度
+    /// </summary>
+    /// <returns>jaro-winkler距离: [0,1]</returns>
+    public static double Similarity(string s1, string s2)
+    {
+        if(s1.Length > s2.Length)
+            (s1, s2) = (s2, s1);
+        int n = s1.Length, m = s2.Length, range = Math.Max(m / 2 - 1, 0);
+        int match = 0, swap = 0;
+        for (var i = 0; i < n; i++)
+        {
+            if (s1[i] == s2[i])
+            {
+                match++;
+                continue;
+            }
+            int matched = 0, swapped = 0;
+            for(var j = Math.Max(0, i-range); j < Math.Min(m, i+range+1); j++)
+                if (i!= j && s1[i] == s2[j])
+                {
+                    matched = 1;
+                    if (i != j && j < n && s1[j] == s2[i])
+                        swapped = 1;
+                    if(swapped == 1)
+                        break;
+                }
+            swap += swapped;
+            match += matched;
+        }
+        if (match == 0)
+            return 0;
+        return (match / (double)n + match / (double)m + (match - swap / 2.0) / match) / 3.0;
+    }
 }
