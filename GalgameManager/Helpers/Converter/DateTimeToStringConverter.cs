80f9c873076806bb8ec22b6b6aea23bc3c80061a
26071 <2607131406@..qcom>
2023-08-13 22:38:56 +0800

    完成了发布时间的获取与排序 顺便修理了一下Bug

---
+++ GalgameManager/Helpers/Converter/DateTimeToStringConverter.cs
@@ -0,0 +1,14 @@
+﻿using System.Globalization;
+using GalgameManager.Models;
+using Microsoft.UI.Xaml.Data;
+
+namespace GalgameManager.Helpers.Converter;
+
+public class DateTimeToStringConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, string language) => 
+        value is DateTime dateTime && dateTime != DateTime.MinValue ? dateTime.ToString("yyyy-MM-dd") : Galgame.DefaultString;
+
+    public object ConvertBack(object value, Type targetType, object parameter, string language) => 
+        DateTime.ParseExact((string)value, "yyyy-MM-dd", CultureInfo.InvariantCulture);
+}
