d3d5f8d4e85d74d21e00957eb375c33795465864
26071 <2607131406@..qcom>
2023-08-16 23:56:37 +0800

    动画修复

--- GalgameManager/Views/GalgamePage.xaml.cs
+++ GalgameManager/Views/GalgamePage.xaml.cs
@@ -4,6 +4,7 @@ using GalgameManager.ViewModels;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Navigation;
 using CommunityToolkit.WinUI.UI.Animations;
+using Microsoft.UI.Xaml;
 
 namespace GalgameManager.Views;
 
@@ -20,7 +21,12 @@ public sealed partial class HomeDetailPage : Page
         InitializeComponent();
         //由于某种奇怪的Bug，直接在DetailImage处指定animations:Connected.Key=“galgameItem”没有动画效果
         //所以采用这种写法
-        this.RegisterElementForConnectedAnimation("galgameItem", DetailImage); 
+        this.RegisterElementForConnectedAnimation("galgameItem", DetailImage, 
+            new []{InfoPanel}); 
+        this.AttachAnchorElementForConnectedAnimation(InfoPanel, DetailImage);
+
+
+
     }
 
     protected override void OnNavigatingFrom(NavigatingCancelEventArgs e)
