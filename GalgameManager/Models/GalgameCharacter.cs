6732a2a5273d5b1df5c0232b2714a59d2040df00
26071 <2607131406@..qcom>
2024-02-24 22:58:47 +0800

    完善bgm角色信息提取

--- GalgameManager/Models/GalgameCharacter.cs
+++ GalgameManager/Models/GalgameCharacter.cs
@@ -7,6 +7,25 @@ public partial class GalgameCharacter: ObservableObject
 {
     [ObservableProperty] private string _name = "";
     [ObservableProperty] private string _relation = "";
+    [ObservableProperty] private string _previewImagePath = Galgame.DefaultImagePath;
     [ObservableProperty] private string _imagePath = Galgame.DefaultImagePath;
+    [ObservableProperty] private string _summary = "";
+    [ObservableProperty] private Gender _gender = Models.Gender.Unknown;
+    [ObservableProperty] private int? _birthYear;
+    [ObservableProperty] private int? _birthMon;
+    [ObservableProperty] private int? _birthDay;
+    [ObservableProperty] private string? _bloodType;
+    [ObservableProperty] private string? _height;
+    [ObservableProperty] private string? _weight;
+    [ObservableProperty] private string? _bWH;
+    [JsonIgnore] public string? PreviewImageUrl;
     [JsonIgnore] public string? ImageUrl;
+
+}
+
+public enum Gender
+{
+    Unknown,
+    Male,
+    Female
 }
