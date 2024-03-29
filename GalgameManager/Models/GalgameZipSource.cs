d4efb39c4e19ac29898921c9222929b4c19f76cd
26071 <2607131406@..qcom>
2024-03-29 21:39:17 +0800

    基础压缩查找

--- GalgameManager/Models/GalgameZipSource.cs
+++ GalgameManager/Models/GalgameZipSource.cs
@@ -1,6 +1,9 @@
 ﻿using System.Collections.ObjectModel;
+using System.Text.RegularExpressions;
 using GalgameManager.Contracts.Models;
+using GalgameManager.Contracts.Services;
 using GalgameManager.Core.Contracts.Services;
+using GalgameManager.Enums;
 using GalgameManager.Helpers;
 using GalgameManager.Models.BgTasks;
 using GalgameManager.Services;
@@ -12,7 +15,7 @@ namespace GalgameManager.Models;
 
 public class GalgameZipSource : GalgameSourceBase
 {
-    [JsonIgnore] public bool IsUnpacking;
+    public static string FxRegex = @"(?<=\\)(?<name>[^\.\\]+)(?:(\.part1)?\.(zip|rar|7z))$";
     public override SourceType GalgameSourceType => SourceType.LocalZip;
 
     public GalgameZipSource(string path, IDataCollectionService<Galgame> service): base(path, service)
@@ -23,5 +26,33 @@ public class GalgameZipSource : GalgameSourceBase
     {
         return path[..path.LastIndexOf('\\')] == Path ;
     }
-
+    
+    public async override IAsyncEnumerable<(Galgame?, string)> ScanAllGalgames()
+    {
+        ILocalSettingsService localSettings = App.GetService<ILocalSettingsService>();
+        
+        var searchSubFolder = await localSettings.ReadSettingAsync<bool>(KeyValues.SearchChildFolder);
+        var maxDepth = searchSubFolder ? await localSettings.ReadSettingAsync<int>(KeyValues.SearchChildFolderDepth) : 1;
+        
+        Queue<(string Path, int Depth)> pathToCheck = new();
+        pathToCheck.Enqueue((Path, 0));
+        while (pathToCheck.Count > 0)
+        {
+            var (currentPath, currentDepth) = pathToCheck.Dequeue();
+            
+            foreach (var f in Directory.GetFiles(currentPath))
+            {
+                Match m = Regex.Match(f, FxRegex);
+                if (m.Success)
+                {
+                    yield return (new 
+                        Galgame(GalgameSourceType, m.Groups["name"].Value, f), "");
+                }
+                
+            }
+            if (currentDepth == maxDepth) continue;
+            foreach (var subPath in Directory.GetDirectories(currentPath))
+                pathToCheck.Enqueue((subPath, currentDepth + 1));
+        }
+    }
 }
