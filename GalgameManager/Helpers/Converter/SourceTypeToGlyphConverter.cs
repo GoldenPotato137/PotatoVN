baa3a8a9bebd4df77e7b517ba371e19b4205ec1d
26071 <2607131406@..qcom>
2024-03-31 16:19:47 +0800

    界面优化

---
+++ GalgameManager/Helpers/Converter/SourceTypeToGlyphConverter.cs
@@ -0,0 +1,19 @@
+﻿using GalgameManager.Enums;
+using GalgameManager.Models.Sources;
+using Microsoft.UI.Xaml.Data;
+
+namespace GalgameManager.Helpers.Converter;
+internal class SourceTypeToGlyphConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, string language)
+    {
+        return value switch
+        {
+            GalgameSourceType.LocalFolder => "\uE8B7",
+            GalgameSourceType.LocalZip => "\uF012",
+            _ => ""
+        };
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, string language) => GalgameSourceType.UnKnown; //不需要
+}
