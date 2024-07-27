3a3e6517e9eff47fb346000cc84e4aed51bf3fe4
26071 <2607131406@.qqcom>
2024-07-27 18:20:30 +0800

    feat:发布日期修改完善

---
+++ GalgameManager/Helpers/Converter/DateTimeToDateTimeOffsetConverter.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.UI.Xaml.Data;
+using DateTimeOffset = System.DateTimeOffset;
+
+namespace GalgameManager.Helpers.Converter;
+
+public class DateTimeToDateTimeOffsetConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object parameter, string language)
+    {
+        if (value is DateTime d)
+        {
+            if (d == DateTime.MinValue) return null;
+            return new DateTimeOffset(d);
+        }
+        throw new ArgumentException($"{value} is not DateTime");
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object parameter, string language)
+    {
+        if (value == null) return DateTime.MinValue;
+        if (value is DateTimeOffset d) return d.DateTime;
+        throw new ArgumentException($"{value} is not DateTimeOffset");
+    }
+}
