1eb06c2dd30584847a30b232d581fdffa53e363d
goldenpotato137 <goldenpotato137@gmail.com>
2024-10-21 15:39:30 +0800

    to(app): GalgameSource改造part7-1，新增压缩游戏后台任务

---
+++ GalgameManager/Models/BgTasks/PackGameTask.cs
@@ -0,0 +1,40 @@
+﻿using GalgameManager.Helpers;
+using SharpCompress.Archives;
+using SharpCompress.Archives.Zip;
+using SharpCompress.Common;
+
+namespace GalgameManager.Models.BgTasks;
+
+public class PackGameTask : BgTaskBase
+{
+    public string GamePath;
+    public string ZipPath;
+
+    public PackGameTask(string gamePath, string zipPath)
+    {
+        GamePath = gamePath;
+        ZipPath = zipPath;
+    }
+
+    protected override Task RecoverFromJsonInternal() => Task.CompletedTask;
+
+    protected override Task RunInternal()
+    {
+        if (Path.Exists(ZipPath))
+        {
+            if (StartFromBg) File.Delete(ZipPath);
+            else throw new PvnException("PackGameTask_ZipExist".GetLocalized(ZipPath));
+        }
+        if (!Path.Exists(GamePath))
+            throw new PvnException("PackGameTask_GamePathNotExist".GetLocalized(GamePath));
+
+        return Task.Run(() =>
+        {
+            using ZipArchive archive = ZipArchive.Create();
+            archive.AddAllFromDirectory(GamePath);
+            archive.SaveTo(ZipPath, CompressionType.Deflate);
+        });
+    }
+
+    public override string Title => "PackGameTask_Title".GetLocalized();
+}
